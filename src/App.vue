8ead89fbcd2cba017bea6efd4469e82d710fca3f
Ashadozzaman <shovoua@gmail.com>
2023-10-21 20:32:19 +0600

    how to work non props attribute

--- src/App.vue
+++ src/App.vue
@@ -1,139 +1,36 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
-    <!-- <div>
-      <p>My Rating: {{ myRating }}</p>
-      <the-ratting v-model="myRating"></the-ratting>
-    </div> -->
-    <!-- <ProductCard
-      v-for="product in products"
-      :key="product.title"
-      :product="product"
-      @buy-now-clicked="buyClicked"
-      @buy-add-to-cart-clicked="addToCartClicked"
-      @toogle-clicked-like="toogleClickedLike"
-    ></ProductCard> -->
-    <!-- <TheCard cardTitle="About Me ">
-      <template v-slot:default>
-        <p>Hi I am Ashadozzaman Shvou</p>
-      </template>
-      <template v-slot:footer>
-        <a href="http://">Learn More</a>
-      </template>
-    </TheCard> -->
-    <!-- <TheCard cardTitle="Apple iPhone 14 Pro">
-      <template v-slot:default>
-        <img
-          src="https://adminapi.applegadgetsbd.com/storage/media/large/iPhone-14-Pro-Max-9907.jpg"
-          alt=""
-        />
-      </template>
-
-      <template v-slot:footer>
-        <button>Buy Now</button>
-        <button>Add Cart</button>
-      </template>
-    </TheCard> -->
-    <!-- <TheCard cardTitle="Service">
-      <u>
-        <li>Html</li>
-        <li>Slot</li>
-        <li>Props</li>
-      </u>
-    </TheCard> -->
-    <!-- <FirstComponentVue 
-            v-for="contact in contacts"
-            :key="contact.name"
-            :name="contact.name" 
-            :website-address="contact.websiteAddress" 
-            :phone="contact.phone">
-    </FirstComponentVue>-->
-  
     <div class="container">
-      <credit-card 
-       v-model:nameOnCard="nameOnCard"
-       v-model:cardNumber="cardNumber"
-       v-model:expire="expire"
-       v-model:cvv="cvv"></credit-card>
+      <the-comment v-model="myComment"
+      buttonText="Continue"
+      class="shadowed"
+      placeholder="Enter Comment"
+      @click="handleClick" 
+      ></the-comment>
       <hr>
-      <p>
-        Name: {{ nameOnCard }} <br>
-        Card Number: {{ cardNumber }} <br>
-        Expire: {{ expire }} <br>
-        CVV: {{ cvv }} <br>
-      </p>
+      <p>{{myComment}}</p>
     </div>
   </div>
 </template>
 
 <script>
-// import FirstComponentVue from './FirstComponent.vue';
-// import TheCard from "./TheCard.vue";
-// import ProductCard from "./ProductCard.vue";
-// import TheRatting from "./TheRatting.vue";
-import CreditCard from "./CreditCard.vue";
+import TheComment from "./TheComment.vue";
 
 export default {
   data() {
     return {
-      msg: "Vue V-Model",
-      nameOnCard:"Ashadozzaman",
-      cardNumber:"",
-      expire:"",
-      cvv:"",
-      // contacts: [{
-      //   name: "Ashadozzaman",
-      //   phone: "017111111",
-      //   websiteAddress: "ashadozzaman.com",
-      // },
-      // {
-      //   name: "Shvou",
-      //   phone: "018111111",
-      //   websiteAddress: "shvoua.com",
-      // },
-      // {
-      //   name: "Karim",
-      //   phone: "019111111",
-      // },
-      // ],
-      // products: [{
-      //   title: "Apple 14 pro MAX",
-      //   thumbnail: "https://adminapi.applegadgetsbd.com/storage/media/large/iPhone-14-Pro-Max-9907.jpg",
-      //   price: "$19345",
-      //   like: true
-      // },
-      // {
-      //   title: "Samsung A20",
-      //   thumbnail: "https://www.mobilebd.co/wp-content/uploads/2020/08/Samsung-Galaxy-A20-Official-image.jpg",
-      //   price: "$11345",
-      //   like: false
-      // },
-      // {
-      //   title: "MI Phone",
-      //   thumbnail: "https://fdn2.gsmarena.com/vv/bigpic/xiaomi-mi-a2-lite.jpg",
-      //   price: "$1934",
-      //   like: true
-      // },
-      // ],
+      msg: "Vue Non-Prop",
+      myComment: "Mr.",
     };
   },
   methods: {
-    buyClicked() {
-      console.log("Buying....");
-    },
-    addToCartClicked(product) {
-      console.log("Add to cart...", product);
-    },
-    toogleClickedLike(product) {
-      product.like = !product.like;
+    handleClick(){
+      console.log('clicked');
     }
   },
   components: {
-    // FirstComponentVue,
-    // TheCard,
-    // ProductCard,
-    // TheRatting
-    CreditCard
+    TheComment
   }
 };
 </script>
---
+++ src/TheComment.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="credit-card">
+        <label>Comment</label>
+        <textarea cols="3" rows="3"
+        :value="modelValue"
+        @input="handleInput"
+        v-bind="$attrs" ></textarea>
+        <button class="text-right">{{buttonText}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        modelValue:{
+            type: String,
+            default: ''
+        },
+        buttonText: {
+            type: String,
+            default: 'Ok'
+        }
+    }, 
+    // inheritAttrs:false,
+    mounted(){
+        console.log(this.$attrs);
+    },
+    methods: {
+        handleInput(e) {
+            this.$emit("update:modelValue", e.target.value);
+        }
+    },
+}
+</script>
+
+<style>
+.credit-card {
+    width: 300px;
+    border: 1px solid gray;
+    padding: 22px;
+    border-radius: 5px;
+    background-color: rgb(255, 254, 247);
+    color: rgb(82, 82, 82);
+    min-height: 111px;
+}
+
+.text-right {
+    float: right;
+}
+
+textarea {
+    padding: 7px 9px;
+    margin-bottom: 11px;
+    border-radius: 3px;
+    width: 100%;
+    border: 1px solid gray;
+}
+
+.row {
+    display: flex;
+}
+
+.col-md-6 {
+    width: 85%;
+}
+
+.shadowed{
+    box-shadow: 0 0 3px 2px #111;
+}
+</style>
